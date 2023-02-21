ee61d8c2f50ce861f940f5981bf4f505565b66f3
driccio <damien.riccio@4sh.fr>
2023-02-21 14:40:03 +0100

    bla

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -8,7 +8,7 @@
 #define MASTER_LEFT
 
 #ifdef RGB_MATRIX_ENABLE
-#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses ieuieuieu
 // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 // #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 #   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
